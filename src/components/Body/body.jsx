01601f50903df1dd250fae151bd59892bf0cb51a
Edson <edcortes@terpmail.umd.edu>
2023-01-19 11:48:08 -0500

    added random image generation as a placeholder

--- src/components/Body/body.jsx
+++ src/components/Body/body.jsx
@@ -2,18 +2,33 @@ import React, {useState, useRef} from 'react'
 import './body.css'
 
 const Body = () => {
-  const [joke, setJoke] = useState("")
-  const form = useRef()
-  const getJoke = (e) => {
+  const [joke, setJoke] = useState("");
+  const [image, setImage] = useState("");
+  const form = useRef();
+  const buttonSubmit = (e) => {
     e.preventDefault();
+    getJoke();
+    getRandomImage();
+    e.target.reset();
 
+  }
+ 
+  const getJoke = () => {
     fetch("https://official-joke-api.appspot.com/random_joke").then(
       (response) => response.json())
       .then((data) => {
         setJoke("Random Joke for no reason:  " + data.setup + " ... " + data.punchline);
       });
+  }
 
-    e.target.reset();
+  const getRandomImage = () => {
+    const endpoint = "https://api.unsplash.com/photos/random/?client_id=XxGyPIroWhkJGrsv-0rEdcWEKTxCBN9pzg3YK_84RsA";
+    fetch(endpoint).then(
+      (response) => response.json()
+      ).then((data) => {
+        setImage(data.urls.regular)
+        console.log(data.urls.regular)
+      });
   }
 
   return (
@@ -21,15 +36,15 @@ const Body = () => {
       <h2> Enter prompt below</h2>
         
       <div>
-        <form Ref={form} onSubmit={getJoke}>
+        <form Ref={form} onSubmit={buttonSubmit}>
           <input type='text' name='prompt' placeholder='Enter Prompt here' required/>
           <button type='submit' className='btn'>Generate</button>
         </form>
 
         <div className="image_box">
-
+          <img className="unsplash_image" alt="" src={image}/>
         </div>
-        
+
         <div className="joke_container">
           {joke}
         </div>
